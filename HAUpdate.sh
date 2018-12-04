c33b4616cc86bd26c541df56f7bea4891a8465e8
Carlo Costanzo <Carlo@ipm.com>
2018-12-04 11:13:08 -0500

    Picked up a tip!
    Picked up a good tip from https://github.com/mf-social/Home-Assistant/issues/595

--- HAUpdate.sh
+++ HAUpdate.sh
@@ -2,5 +2,8 @@
 cd /home/hass/docker_files
 docker-compose pull 
 docker-compose down 
-docker-compose up -d &
+docker-compose up -d
 docker system prune -fa
+docker volume prune -f
+
+docker attach --sig-proxy=false hass
