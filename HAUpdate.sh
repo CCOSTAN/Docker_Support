2785f611af282b668a1130822b5f08f443776398
Carlo Costanzo <Carlo@ipm.com>
2021-07-12 12:23:21 -0400

    REMOVES Ophan containers

--- HAUpdate.sh
+++ HAUpdate.sh
@@ -2,7 +2,7 @@
 cd /home/hass/docker_files
 docker-compose pull 
 docker-compose down 
-docker-compose up -d
+docker-compose up -d --remove-orphans   
 docker system prune -fa
 docker volume prune -f
 docker image prune -fa
