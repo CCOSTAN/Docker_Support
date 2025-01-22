2cf2f119c2bd9972c99b71b61bffc8a1c7fe249d
Carlo Costanzo <2160436+CCOSTAN@users.noreply.github.com>
2025-01-22 13:05:59 -0500

    Only recreate updated containers to minimize downtime

--- HAUpdate.sh
+++ HAUpdate.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+sudo apt-get update && sudo apt-get upgrade
+
+# Navigate to the directory
 cd /home/hass/docker_files
-docker-compose pull 
-docker-compose down 
-docker-compose up -d --remove-orphans   
+
+# Pull the latest images
+docker-compose pull
+
+# Only recreate containers if there are updated images
+docker-compose up -d --remove-orphans --no-recreate
+
+# Clean up unused Docker resources
 docker system prune -fa
 docker volume prune -f
 docker image prune -fa
 
+# Reattach to the container
 docker attach --sig-proxy=false hass
